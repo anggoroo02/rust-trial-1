fd30d760ebe7926d97f1b8b1a6778857c80d4874
anggoro jati <anggorojatipratikto002@gmail.com>
2025-01-03 22:07:52 +0700

    repair mutable vaiable

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ fn main() {
 }
 #[test]
 fn main_bo() {
-    let word: &str="anggoro jati";
+    let mut word: &str="anggoro jati";
     println!("{}",word);
     word ="word has changed";
     println!("{}",word);

70b42f8475f2f770d9da59f59368f399deac1457
anggoro jati <anggorojatipratikto002@gmail.com>
2025-01-04 22:21:54 +0700

    tipe data: boolean (true, false)

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,14 @@ fn main() {
 // test untuk tipe data
 #[test]
 fn data_type() {
-    let angka:u32=32;
+    let mut angka:i64=-2006;
+    angka +=300;
     println!("isi dari variable angka adalah: {}", angka)
+}
+#[test]
+fn boolean_prove() {
+    let mut t: bool = true;
+    println!("nilai boolean dari t: {}", t);
+    t = false;
+    println!("nilai boolean dari t: {}", t);
 }
